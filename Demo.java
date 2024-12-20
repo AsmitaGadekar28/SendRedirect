5beeb834b65fcc52ef1323aea33c2d6c8a172d62
AsmitaGadekar28 <asmitagadekar28@gmail.com>
2024-12-20 14:56:16 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,21 @@
+package com.tka;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/submit")
+public class Demo extends HttpServlet
+{
+      @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	
+    	 String mysearch= req.getParameter("search1");
+    	 System.out.println(mysearch);
+    	 resp.sendRedirect("http://www.google.com/search?q="+mysearch);
+    }
+}
